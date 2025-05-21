fc21b13efc8f7e4d996124a814be1af57ea852aa
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-21 20:26:41 +0300

    fix: added quote icon to images in reviews section

--- app/components/ReviewsCard.jsx
+++ app/components/ReviewsCard.jsx
@@ -4,13 +4,15 @@ import { Subtitle } from "."
 const ReviewsCard = ({ data }) => {
     return (
         <div className="reviews__card">
-            <img
-                className="reviews__card-image"
-                src={data.image}
-                alt=""
-                width={140}
-                height={140}
-            />
+            <div className="reviews__card-quote-image">
+                <img
+                    className="reviews__card-image"
+                    src={data.image}
+                    alt=""
+                    width={140}
+                    height={140}
+                />
+            </div>
             <div className="reviews__card-body">
                 <h3 className="reviews__card-title h5">{data.excerpt}</h3>
                 <p className="reviews__card-text">{data.text}</p>
