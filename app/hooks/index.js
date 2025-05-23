ca747a6d2fb2fedd2e17e58b134542ff1f423c63
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-23 17:12:29 +0300

    add: useTabs hook

--- app/hooks/index.js
+++ app/hooks/index.js
@@ -1,2 +1,3 @@
 export {default as useSlider} from './useSlider.js'
 export {default as useMatchMedia} from './useMatchMedia.js'
+export {default as useTabs} from './useTabs.js'
---
+++ app/hooks/useTabs.js
@@ -0,0 +1,7 @@
+import { useSlider } from '.'
+
+function useTabs(tabsData) {
+    return useSlider(tabsData, 1)
+}
+
+export default useTabs
