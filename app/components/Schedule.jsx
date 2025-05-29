5eabb6931bf691596c0f30caca15aaf19c52e588
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-29 19:15:16 +0300

    change: updated Schedule component

--- app/components/Schedule.jsx
+++ app/components/Schedule.jsx
@@ -1,36 +1,36 @@
 import '../styles/components/Schedule.scss'
 
-const Schedule = () => {
+const Schedule = ({ isAlt }) => {
     return (
-        <div className='schedule'>
-            <h3 className='schedule__title h4'>Working Hours</h3>
+        <div className={`schedule ${isAlt ? 'schedule--alt' : ''}`}>
+            <h3 className={`schedule__title ${isAlt ? 'h6' : 'h4'}`}>Work{isAlt ? '' : 'ing'} Hours</h3>
             <dl className='schedule__list'>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Monday</dt>
+                    <dt className='schedule__key'>Monday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Tuesday</dt>
+                    <dt className='schedule__key'>Tuesday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Wednesday</dt>
+                    <dt className='schedule__key'>Wednesday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Thursday</dt>
+                    <dt className='schedule__key'>Thursday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Friday</dt>
+                    <dt className='schedule__key'>Friday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Saturday</dt>
+                    <dt className='schedule__key'>Saturday{isAlt && ':'}</dt>
                     <dd className='schedule__value'><time dateTime="08:00">8am</time>-<time dateTime='19:00'>7pm</time></dd>
                 </div>
                 <div className='schedule__item'>
-                    <dt className='schedule__key'>Sunday</dt>
+                    <dt className='schedule__key'>Sunday{isAlt && ':'}</dt>
                     <dd className='schedule__value'>closed</dd>
                 </div>
             </dl>
