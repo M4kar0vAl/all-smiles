e5818aa45bc4905a9cebac5fe5d5d7899c684c2b
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-29 19:18:36 +0300

    change: updated Logo component

--- app/components/Logo.jsx
+++ app/components/Logo.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router"
 
 import logo from '../assets/logo.svg'
+import logoAlt from '../assets/logo-alt.png'
 import '../styles/components/Logo.scss'
 
-const Logo = () => {
+const Logo = ({ isAlt = false }) => {
     return (
         <Link to="/" className='logo' >
             <img
-                src={logo}
+                src={isAlt ? logoAlt : logo}
                 alt=""
                 className='logo__image'
-                width={135}
-                height={17}
+                width={isAlt ? 105 : 135}
+                height={isAlt ? 110 : 17}
             />
         </Link>
     )
