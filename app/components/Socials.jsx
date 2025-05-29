bbf5503381dd000b51ad3ed3f5dc32529ef217bd
Alexey Makarov <alex.makarov2290@gmail.com>
2025-05-29 19:17:45 +0300

    add: Socials component

---
+++ app/components/Socials.jsx
@@ -0,0 +1,48 @@
+import linkedInIcon from '../assets/socials/linked_in_icon.svg'
+import twitterIcon from '../assets/socials/twitter_icon.svg'
+import facebookIcon from '../assets/socials/facebook_icon.svg'
+import '../styles/components/Socials.scss'
+
+const socials = [
+    {
+        id: 1,
+        link: 'https://example.com',
+        image: linkedInIcon,
+    },
+    {
+        id: 2,
+        link: 'https://example.com',
+        image: twitterIcon,
+    },
+    {
+        id: 3,
+        link: 'https://example.com',
+        image: facebookIcon,
+    },
+]
+
+const Socials = () => {
+    return (
+        <ul className='soc1als'>
+            {
+                socials.map(data => 
+                    <li className="soc1als-item" key={data.id}>
+                        <a
+                            className='soc1als-link'
+                            href={data.link}
+                            target='_blank'
+                        >
+                            <img
+                                className='soc1als-image'
+                                src={data.image}
+                                alt=""
+                            />
+                        </a>
+                    </li>
+                )
+            }
+        </ul>
+    )
+}
+
+export default Socials
